536325ab72f963d4b0aa6bc74be08448e53a479a
Kapeshi Kongolo <Kapeshi.Kongolo@bsg.co.za>
2018-11-06 21:30:57 +0200

    change subdomain for my learning

--- install-openshift.sh
+++ install-openshift.sh
@@ -93,10 +93,10 @@ cd openshift-ansible && git fetch && git checkout release-${VERSION} && cd ..
 cat <<EOD > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-${IP}		$(hostname) console console.${DOMAIN}  
+${IP}		$(hostname) console learning.${DOMAIN}
 EOD
 
-if [ -z $DISK ]; then 
+if [ -z $DISK ]; then
 	echo "Not setting the Docker storage."
 else
 	cp /etc/sysconfig/docker-storage-setup /etc/sysconfig/docker-storage-setup.bk
@@ -167,10 +167,10 @@ if [ "$PVS" = "true" ]; then
 	for i in `seq 1 200`;
 	do
 		DIRNAME="vol$i"
-		mkdir -p /mnt/data/$DIRNAME 
+		mkdir -p /mnt/data/$DIRNAME
 		chcon -Rt svirt_sandbox_file_t /mnt/data/$DIRNAME
 		chmod 777 /mnt/data/$DIRNAME
-		
+
 		sed "s/name: vol/name: vol$i/g" vol.yaml > oc_vol.yaml
 		sed -i "s/path: \/mnt\/data\/vol/path: \/mnt\/data\/vol$i/g" oc_vol.yaml
 		oc create -f oc_vol.yaml
@@ -180,13 +180,13 @@ if [ "$PVS" = "true" ]; then
 fi
 
 echo "******"
-echo "* Your console is https://console.$DOMAIN:$API_PORT"
+echo "* Your console is https://learning.$DOMAIN:$API_PORT"
 echo "* Your username is $USERNAME "
 echo "* Your password is $PASSWORD "
 echo "*"
 echo "* Login using:"
 echo "*"
-echo "$ oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:$API_PORT/"
+echo "$ oc login -u ${USERNAME} -p ${PASSWORD} https://learning.$DOMAIN:$API_PORT/"
 echo "******"
 
-oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:$API_PORT/
+oc login -u ${USERNAME} -p ${PASSWORD} https://learning.$DOMAIN:$API_PORT/
